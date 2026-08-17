a15834de8d724257a8c469e72af769d747369177
rohitxenel <rohit.bamrara@xenelsoft.co.in>
2026-08-17 17:19:23 +0530

    fix: update API_BASE_URL to point to the correct endpoint

--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -4,6 +4,7 @@
 // export const SOCKET_URL = "http://localhost:8082"
 
 
-export const API_BASE_URL = 'https://mobileappdevelopmentindia.com/nas/api/v1';
+// export const API_BASE_URL = 'https://mobileappdevelopmentindia.com/nas/api/v1';
+export const API_BASE_URL = 'https://api.letsnas.com/api/v1';
 // export const IMAGE_URL ="http://34.131.104.9:8082/uploads"
 export const SOCKET_URL = "https://mobileappdevelopmentindia.com"
